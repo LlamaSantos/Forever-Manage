e357ff2cc6b8810fb6d7f46654b8aec1180607a6
James White <james@llamasantos.com>
2012-07-08 22:09:19 -0600

    Removed .idea

--- worksheet.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-
-var inStream = fs.createReadStream("base.json", { encoding: "utf-8" });
-
-var outStream = fs.createWriteStream("temp.json", { encoding: "utf-8" });
-
-inStream.pipe(outStream);
